9c5230ac4ea8221ffbbfa10b767ad2495b2a352a
alex-chulu <chulualexandar@gmail.com>
2023-07-23 09:11:24 -0700

    task 0 fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+""" Pagination function"""
 
+from typing import Tuple
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end index for a given page and page size.
 
