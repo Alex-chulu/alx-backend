b9620d222ec73abd734b837cedf97ac546e8a8d1
alex-chulu <chulualexandar@gmail.com>
2023-07-23 09:13:18 -0700

    task 0 fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -14,11 +14,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Returns:
         tuple: A tuple containing the start index and end index.
     """
-    if page <= 0 or page_size <= 0:
-        raise ValueError("Page and page_size must be positive integers.")
-
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
-
-    return start_index, end_index
+    return ((page - 1) * page_size, page * page_size)
 
