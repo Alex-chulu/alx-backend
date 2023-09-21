a33afddc41881c443a82cf4b5d27fbe923d4349a
Alex-chulu <chulualexander@gmail.com>
2023-09-21 23:07:09 +0000

    fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Render the index.html template."""
-    return render_template('0-index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
